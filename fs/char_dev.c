31fca04603afe7ae7a9d1331f35da8b7c3c72e13
karottc <karottc@gmail.com>
2014-08-21 00:33:19 +0800

    给fs/char_dev.c文件增加注释-(2)--done.

--- fs/char_dev.c
+++ fs/char_dev.c
@@ -58,10 +58,16 @@ static int rw_kmem(int rw,char * buf, int count, off_t * pos)
 	return -EIO;
 }
 
+//// 端口读写操作函数
+// 参数：rw - 读写命令； buf - 缓冲区; cout - 读写字节数; pos - 端口地址。
+// 返回：实际读写的字节数。
 static int rw_port(int rw,char * buf, int count, off_t * pos)
 {
 	int i=*pos;
 
+    // 对于所要求读写的字节数，并且端口地址小于64K时，循环执行单个字节的
+    // 读写操作。若是读命令，则从端口i中读取一个字节内容并放到用户缓冲区中。
+    // 若是写命令，则从用户数据缓冲区中取一字节输出到端口i。
 	while (count-->0 && i<65536) {
 		if (rw==READ)
 			put_fs_byte(inb(i),buf++);
@@ -69,13 +75,16 @@ static int rw_port(int rw,char * buf, int count, off_t * pos)
 			outb(get_fs_byte(buf++),i);
 		i++;
 	}
+    // 然后计算读/写字节数，调整相应读写指针，并返回读/写的字节数。
 	i -= *pos;
 	*pos += i;
 	return i;
 }
 
+//// 内存读写操作函数
 static int rw_memory(int rw, unsigned minor, char * buf, int count, off_t * pos)
 {
+    // 根据内存设备子设备号，分别调用不同的内存读写函数。
 	switch(minor) {
 		case 0:
 			return rw_ram(rw,buf,count,pos);
@@ -92,8 +101,10 @@ static int rw_memory(int rw, unsigned minor, char * buf, int count, off_t * pos)
 	}
 }
 
+// 定义系统中设备种数
 #define NRDEVS ((sizeof (crw_table))/(sizeof (crw_ptr)))
 
+// 字符设备读写函数指针表
 static crw_ptr crw_table[]={
 	NULL,		/* nodev */
 	rw_memory,	/* /dev/mem etc */
@@ -104,10 +115,15 @@ static crw_ptr crw_table[]={
 	NULL,		/* /dev/lp */
 	NULL};		/* unnamed pipes */
 
+// 字符设备读写操作函数
+// 参数：rw - 读写命令；dev - 设备号；buf - 缓冲区; count - 读写字节数；pos - 读写指针。
+// 返回：实际读/写字节数
 int rw_char(int rw,int dev, char * buf, int count, off_t * pos)
 {
 	crw_ptr call_addr;
 
+    // 如果设备号超出系统设备数，则返回出错码。如果该设备没有对应的读/写函数，也
+    // 返回出错码。否则调用对应设备的读写操作函数，并返回实际读/写的字节数。
 	if (MAJOR(dev)>=NRDEVS)
 		return -ENODEV;
 	if (!(call_addr=crw_table[MAJOR(dev)]))
